180e6538d885802c6a0ba1a353fba4fb18279b41
makv3r <makv3r@gmail.com>
2018-11-11 04:45:24 +0200

    HW2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,62 +3,79 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
 
+    int size() {
+        return size;
+    }
+
     void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, null);
         size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(r.uuid)) return;
+        if (size == storage.length) { // Проверку на переполнение
+            System.out.println("Stop It! Size will exceed limit.");
         }
-        storage[size] = r;
-        size++;
+        else if (search(r.uuid) != -1) { // Проверку на повторение
+            System.out.println("Resume already exist.");
+        }
+        else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    void update(Resume r, String updateTo) {
+        int i = search(r.uuid);
+        if (i == -1) {
+            System.out.println("Resume not found.");
+        }
+        else {
+            storage[i].uuid = updateTo;
+        }
+    }
+
+    int search(String uuid) { // Избавляет от дублирования в коде ArrayStorage
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
-            }
+        int i = search(uuid);
+
+        if (i == -1) {
+            System.out.println("Resume not found.");
+            return null;
         }
-        return null;
+        else {
+            return storage[i];
+        }
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                for (int j = i; j < size - 1; j++) {
-                    storage[j] = storage[j + 1];
-                }
-                storage[size - 1] = null;
-                size--;
-                break;
-            }
+        int i = search(uuid);
+
+        if (i == -1) {
+            System.out.println("Resume not found.");
+        }
+        else {
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
     }
 
-    //save, get, delete, size, clear, getAll
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        Resume[] tmp = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            tmp[i] = storage[i];
-        }
-        return tmp;
-        //return Arrays.copyOfRange(storage, 0, this.size());
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
-    int size() {
-        return size;
-    }
 }
