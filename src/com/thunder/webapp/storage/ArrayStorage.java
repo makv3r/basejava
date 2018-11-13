8dfe1b341890057fb7cb696de1305090e5223dd9
makv3r <makv3r@gmail.com>
2018-11-13 23:31:57 +0200

    HW2 Comments Edited

--- src/com/thunder/webapp/storage/ArrayStorage.java
+++ src/com/thunder/webapp/storage/ArrayStorage.java
@@ -26,7 +26,7 @@ public class ArrayStorage {
         if (size == MAX_SIZE) { // Проверку на переполнение
             System.out.println("Stop It! Size will exceed limit.");
         } else if (getIndex(r.getUuid()) != -1) { // Проверку на повторение
-            System.out.println("com.thunder.webapp.model.Resume already exist.");
+            System.out.println("Resume already exist.");
         } else {
             storage[size] = r;
             size++;
@@ -36,7 +36,7 @@ public class ArrayStorage {
     public void update(Resume r) {
         int i = getIndex(r.getUuid());
         if (i == -1) {
-            System.out.println("com.thunder.webapp.model.Resume not found.");
+            System.out.println("Resume not found.");
         } else {
             storage[i] = r;
         }
@@ -46,7 +46,7 @@ public class ArrayStorage {
         int i = getIndex(uuid);
 
         if (i == -1) {
-            System.out.println("com.thunder.webapp.model.Resume not found.");
+            System.out.println("Resume not found.");
         } else {
             storage[i] = storage[size - 1];
             storage[size - 1] = null;
@@ -58,7 +58,7 @@ public class ArrayStorage {
         int i = getIndex(uuid);
 
         if (i == -1) {
-            System.out.println("com.thunder.webapp.model.Resume not found.");
+            System.out.println("Resume not found.");
             return null;
         } else {
             return storage[i];
@@ -69,7 +69,7 @@ public class ArrayStorage {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
-    private int getIndex(String uuid) { // Избавляет от дублирования в коде com.thunder.webapp.storage.ArrayStorage
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
